5a0aaef9afae9e85987855be989affa2f881e135
stefkudla <stefankudla@gmail.com>
2022-08-20 20:22:22 -0400

    Backend with pokemon data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ import { withTRPC } from "@trpc/next";
 import type { AppRouter } from "@/backend/router";
 
 function getBaseUrl() {
-  if (process.browser) return ""; // Browser should use current path
+  if (typeof window) return ""; // Browser should use current path
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,40 @@
 import type { NextPage } from "next";
 import { trpc } from "@/utils/trpc";
 import { getOptionsForVote } from "@/utils/getRandomPokemon";
+import { useState } from "react";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-  const [first, second] = getOptionsForVote();
+  const [ids, updateIds] = useState(getOptionsForVote());
+
+  const [first, second] = ids;
+
+  const firstPokemon = trpc.useQuery(["get-pokemon-by-id", { id: first }]);
+  const secondPokemon = trpc.useQuery(["get-pokemon-by-id", { id: second }]);
+
+  if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
+
   return (
     <div className="h-screen w-creen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-200">{first}</div>
+        <div className="relative w-64 h-64 flex flex-col-reverse ">
+          <Image src={firstPokemon.data?.sprites.front_default} layout="fill" />
+          <div className="text-xl text-center capitalize">
+            {firstPokemon.data.name}
+          </div>
+        </div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200">{second}</div>
+        <div className="relative w-64 h-64 flex flex-col-reverse">
+          <Image
+            src={secondPokemon.data?.sprites.front_default}
+            layout="fill"
+          />
+          <div className="text-xl text-center capitalize">
+            {secondPokemon.data.name}
+          </div>
+        </div>
       </div>
     </div>
   );
